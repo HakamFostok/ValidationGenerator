339e3207b5f7443c70bd40ad6efc008af5cfe727
Emirhan Aksoy <53901858+EmirhanAksoy@users.noreply.github.com>
2023-09-28 00:17:56 +0300

    Changes

---
+++ src/ValidationGenerator/ValidationGenerator.Core/Concrete/NotNullValidationGenerator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Diagnostics;
+
+namespace ValidationGenerator.Core.Concrete;
+
+[Generator]
+public class NotNullValidationGenerator : ISourceGenerator
+{
+    public NotNullValidationGenerator()
+    {
+
+    }
+    public void Execute(GeneratorExecutionContext context)
+    {
+
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        Debugger.Launch();
+        context.RegisterForSyntaxNotifications(() =>
+        new AttributeSyntaxReceiver<NotNullAttribute>());
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class NotNullAttribute : Attribute
+{
+}
---
+++ src/ValidationGenerator/ValidationGeneratorLab.cs/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Diagnostics.CodeAnalysis;
+
+User user = new();
+user.Id = null;
+user.Validate();
+
+
+Console.WriteLine("Test");
+
+
+public class User : ValidationMarker
+{
+    [NotNull]
+    public string? Id { get; set; } = string.Empty;
+    public string? Name { get; set; } = string.Empty;
+    public string? Email { get; set; } = string.Empty;
+    public string? Password { get; set; } = string.Empty;
+}
+
+public class ValidationMarker
+{
+    public void Validate()
+    {
+
+    }
+
+    public Task ValidateAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
+
